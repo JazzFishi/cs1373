950b3ef8ebbc48cd259b42abc50204d6cf6e051a
Jazzuma "Ahab" Fishiki <88769718+JazzFishi@users.noreply.github.com>
2026-09-01 16:53:47 +0700

    Add files via upload

---
+++ W4/W4_ass1.java
@@ -0,0 +1,8 @@
+class  W4_ass1
+{//being class
+	public static void main(String[] args) 
+	{//being main
+		System.out.println("Hello World!");
+		System.out.print("Hello World!");
+	}//end main
+}//end class
