50201675fd6853a3074dd621b66bce212c55a4c7
Jazzuma "Ahab" Fishiki <88769718+JazzFishi@users.noreply.github.com>
2026-09-01 16:50:58 +0700

    Delete W4_ass1.java

--- W4_ass1.java
+++
@@ -1,8 +0,0 @@
-class  W4_ass1
-{//being class
-	public static void main(String[] args) 
-	{//being main
-		System.out.println("Hello World!");
-		System.out.print("Hello World!");
-	}//end main
-}//end class
